1a189bf0585b7325436c08ac2cfffa25eb158ae9
Leen nusir <leen.nsser99@gmail.com>
2021-05-19 22:13:47 +0300

    Notification

--- app/src/main/java/com/example/agriculturalproject/HumiditySystem.java
+++ app/src/main/java/com/example/agriculturalproject/HumiditySystem.java
@@ -2,7 +2,17 @@ package com.example.agriculturalproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +30,9 @@ import org.jetbrains.annotations.NotNull;
 public class HumiditySystem extends AppCompatActivity {
     TextView plant_Humidity , plant_Soil_Moisture ,Fan ,AC;
     DatabaseReference boxes ;
+    public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
+    private final static String default_notification_channel_id = "default" ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +50,9 @@ public class HumiditySystem extends AppCompatActivity {
                 plant_Soil_Moisture.setText(box.getSoil_Moisture());
                 Fan.setText(box.getFan());
                 AC.setText(box.getAC());
+                if (box.getHumidity().equals("10")){
+                    addNotification();
+                }
             }
 
             @Override
@@ -47,4 +63,26 @@ public class HumiditySystem extends AppCompatActivity {
 
 
     }
-}
+    private void addNotification() {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(HumiditySystem. this,
+                default_notification_channel_id )
+                .setSmallIcon(R.drawable. ic_launcher_foreground )
+                .setContentTitle( "Test" )
+                .setContentText( "Hello! This is my first push notification" );
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context. NOTIFICATION_SERVICE ) ;
+        if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
+            int importance = NotificationManager. IMPORTANCE_HIGH ;
+            NotificationChannel notificationChannel = new
+                    NotificationChannel( NOTIFICATION_CHANNEL_ID , "NOTIFICATION_CHANNEL_NAME" , importance) ;
+            notificationChannel.enableLights( true ) ;
+            notificationChannel.setLightColor(Color. RED ) ;
+            notificationChannel.enableVibration( true ) ;
+            notificationChannel.setVibrationPattern( new long []{ 100 , 200 , 300 , 400 , 500 , 400 , 300 , 200 , 400 }) ;
+            mBuilder.setChannelId( NOTIFICATION_CHANNEL_ID ) ;
+            assert mNotificationManager != null;
+            mNotificationManager.createNotificationChannel(notificationChannel) ;
+        }
+        assert mNotificationManager != null;
+        mNotificationManager.notify(( int ) System. currentTimeMillis (), mBuilder.build()) ;
+    }
+    }
