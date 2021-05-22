2007d3d95b882a49e08b178185ee486d7894f10d
haitham <2017901147@ses.yu.edu.jo>
2021-05-22 16:11:04 +0300

    adding 2 pages

--- app/src/main/java/com/example/agriculturalproject/contactus.java
+++ app/src/main/java/com/example/agriculturalproject/contactus.java
@@ -1,72 +1,50 @@
 package com.example.agriculturalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.agriculturalproject.GlobalClasses.Global;
-
-public class Monitor extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+public class contactus extends AppCompatActivity {
     DrawerLayout drawerLayout ;
-    TextView nav_email ,nav_name ;
+    TextView nav_email,nav_name;
 
-    CardView card3 , card4 ,card5 , card6 ;
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monitor);
-
+        setContentView(R.layout.activity_contactus);
+        getSupportActionBar().hide();
         drawerLayout = findViewById(R.id.main_drawer_layout);
         nav_email = findViewById(R.id.txt_view_email);
-        nav_name = findViewById(R.id.txt_view_name);
-        card3 = findViewById(R.id.cardViewWater);
-        card4 = findViewById(R.id.cardViewHumidity);
-        card5 = findViewById(R.id.cardViewTemperature);
-        card6 = findViewById(R.id.cardViewLight);
-
-
         nav_email.setText(Global.currentUser.getEmail());
+        nav_name = findViewById(R.id.txt_view_name);
         nav_name.setText(Global.currentUser.getFirstName() + " "+ Global.currentUser.getLastName() );
-        getSupportActionBar().hide(); // hide the title bar
-        Toast.makeText(this, Global.currentBoxes.getName(), Toast.LENGTH_SHORT).show();
-
-        card3.setOnClickListener(new View.OnClickListener() {
+        BottomNavigationView btmNav = findViewById(R.id.bottom_nav_profile);//take FROM layout(activity profile.xml)
+        btmNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Monitor.this,WaterSystem.class));
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.back_profile://back to last page
+                        //take from menu(bar_profile.xml)
+                        finish();
+
+                        break;
+
+
+                }
+
             }
+
         });
-        card4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Monitor.this, HumiditySystem.class));
-            }
-        });
-        card5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Monitor.this, TemperatureSystem.class));
-            }
-        });
-        card6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Monitor.this, LightSystem.class));
-            }
-        });
-
-
-
-
-
 
     }
     public void OpenMenu(View view){
@@ -84,14 +62,14 @@ public class Monitor extends AppCompatActivity {
     }
     public void Clickprivacypolicy(View view){redirectActivity(this,privacypolicy.class);}
     public void Clickcontactus(View view){redirectActivity(this,contactus.class);}
+
     private void redirectActivity(Activity activity , Class aClass) {
         Intent obj = new Intent(activity,aClass);
         obj.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         activity.startActivity(obj);
     }
-
     public void ClickHome(View view){
-        redirectActivity(this,MainHome.class);
+        redirectActivity(this, MainHome.class);
     }
     public void ClickLogout(View view){
         redirectActivity(this,MainActivity.class);
@@ -109,4 +87,8 @@ public class Monitor extends AppCompatActivity {
         super.onPause();
         closeDrawer(drawerLayout);
     }
+
+
 }
+
+
--- app/src/main/java/com/example/agriculturalproject/privacypolicy.java
+++ app/src/main/java/com/example/agriculturalproject/privacypolicy.java
@@ -1,49 +1,50 @@
 package com.example.agriculturalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.agriculturalproject.GlobalClasses.Global;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainHome extends AppCompatActivity {
+import com.example.agriculturalproject.GlobalClasses.Global;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class privacypolicy extends AppCompatActivity {
     DrawerLayout drawerLayout ;
     TextView nav_email,nav_name;
 
-    CardView card1 , card2  ;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_home);
+        setContentView(R.layout.activity_privacypolicy);
         getSupportActionBar().hide();
         drawerLayout = findViewById(R.id.main_drawer_layout);
-
         nav_email = findViewById(R.id.txt_view_email);
         nav_email.setText(Global.currentUser.getEmail());
         nav_name = findViewById(R.id.txt_view_name);
         nav_name.setText(Global.currentUser.getFirstName() + " "+ Global.currentUser.getLastName() );
-        card1 = findViewById(R.id.cardViewWater);
-        card2 = findViewById(R.id.cardViewHumidity);
+        BottomNavigationView btmNav = findViewById(R.id.bottom_nav_profile);//take FROM layout(activity profile.xml)
+        btmNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.back_profile://back to last page
+                        //take from menu(bar_profile.xml)
+                        finish();
 
-        card1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainHome.this,list_box.class));
+                        break;
+
+
+                }
+
             }
-        });
-        card2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainHome.this, PlantsList.class));
-            }
+
         });
 
     }
@@ -52,7 +53,7 @@ public class MainHome extends AppCompatActivity {
     }
 
     private static void openDrawer(DrawerLayout drawerLayout) {
-    drawerLayout.openDrawer(GravityCompat.START);
+        drawerLayout.openDrawer(GravityCompat.START);
     }
     public void ClickLogo(View view){
         closeDrawer(drawerLayout);
@@ -60,8 +61,10 @@ public class MainHome extends AppCompatActivity {
     public void ClickProfile(View view){
         redirectActivity(this,Profile.class);
     }
-    public void Clickprivacypolicy(View view){redirectActivity(this,privacypolicy.class);}
-    public void Clickcontactus(View view){redirectActivity(this,contactus.class);}
+    public void Clickcontactus(View view)
+    { redirectActivity(this,contactus.class); }
+
+
     private void redirectActivity(Activity activity , Class aClass) {
         Intent obj = new Intent(activity,aClass);
         obj.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -69,7 +72,7 @@ public class MainHome extends AppCompatActivity {
     }
 
     public void ClickHome(View view){
-recreate();
+        redirectActivity(this,MainHome.class);
     }
     public void ClickLogout(View view){
         redirectActivity(this,MainActivity.class);
@@ -77,9 +80,9 @@ recreate();
     }
 
     private static void closeDrawer(DrawerLayout drawerLayout) {
-    if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-        drawerLayout.closeDrawer(GravityCompat.START);
-    }
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
     }
 
     @Override
@@ -87,4 +90,5 @@ recreate();
         super.onPause();
         closeDrawer(drawerLayout);
     }
+
 }
