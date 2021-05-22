8c5b067d229526531a9c9d3b408c092c833ab172
haitham <2017901147@ses.yu.edu.jo>
2021-05-22 18:06:05 +0300

    adding 2 pages

--- app/src/main/java/com/example/agriculturalproject/contactus.java
+++ app/src/main/java/com/example/agriculturalproject/contactus.java
@@ -8,12 +8,18 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.agriculturalproject.GlobalClasses.Global;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class contactus extends AppCompatActivity {
     DrawerLayout drawerLayout ;
     TextView nav_email,nav_name;
@@ -45,6 +51,50 @@ public class contactus extends AppCompatActivity {
             }
 
         });
+        final EditText your_name = (EditText) findViewById(R.id.your_name);
+        final EditText your_email = (EditText) findViewById(R.id.your_email);
+        final EditText your_subject = (EditText) findViewById(R.id.your_subject);
+        final EditText your_message = (EditText) findViewById(R.id.your_message);
+        Button email = (Button) findViewById(R.id.post_message);
+        email.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v){
+                String name = your_name.getText().toString();
+                String email = your_email.getText().toString();
+                String subject = your_subject.getText().toString();
+                String message = your_message.getText().toString();
+                if (TextUtils.isEmpty(name)) {
+                    your_name.setError("Enter Your Name");
+                    your_name.requestFocus();
+                    return;
+                }
+                Boolean onError = false;
+                if (!isValidEmail(email)) {
+                    onError = true;
+                    your_email.setError("Invalid Email");
+                    return;
+                }
+                if (TextUtils.isEmpty(subject)) {
+                    your_subject.setError("Enter Your Subject");
+                    your_subject.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(message)) {
+                    your_message.setError("Enter Your Message");
+                    your_message.requestFocus();
+                    return;
+                }
+                Intent sendEmail = new Intent(android.content.Intent.ACTION_SEND);
+                /* Fill it with Data */
+                sendEmail.setType("plain/text");
+                sendEmail.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"dr.m.karthiik@gmail.com"});
+                sendEmail.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+                sendEmail.putExtra(android.content.Intent.EXTRA_TEXT, "name:" + name + '\n' + "Email ID:" + email + '\n' + "Message:" + '\n' + message);
+                /* Send it off to the Activity-Chooser */
+                startActivity(Intent.createChooser(sendEmail, "Send mail..."));
+            }
+        });
 
     }
     public void OpenMenu(View view){
@@ -58,21 +108,21 @@ public class contactus extends AppCompatActivity {
         closeDrawer(drawerLayout);
     }
     public void ClickProfile(View view){
-        redirectActivity(this,Profile.class);
+        redirectActivityto(this,Profile.class);
     }
-    public void Clickprivacypolicy(View view){redirectActivity(this,privacypolicy.class);}
-    public void Clickcontactus(View view){redirectActivity(this,contactus.class);}
+    public void Clickprivacypolicy(View view){redirectActivityto(this,privacypolicy.class);}
+    public void Clickcontactus(View view){redirectActivityto(this,contactus.class);}
 
-    private void redirectActivity(Activity activity , Class aClass) {
+    private void redirectActivityto(Activity activity , Class aClass) {
         Intent obj = new Intent(activity,aClass);
         obj.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         activity.startActivity(obj);
     }
     public void ClickHome(View view){
-        redirectActivity(this, MainHome.class);
+        redirectActivityto(this, MainHome.class);
     }
     public void ClickLogout(View view){
-        redirectActivity(this,MainActivity.class);
+        redirectActivityto(this,MainActivity.class);
 
     }
 
@@ -88,7 +138,25 @@ public class contactus extends AppCompatActivity {
         closeDrawer(drawerLayout);
     }
 
-
+    public void onResume() {
+        super.onResume();
+        //Get a Tracker (should auto-report)
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    // validating email id
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 }
 
 
